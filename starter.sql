24abaf957779da4fbdfbdaee0144d36010b0ef13
Ali Al-Abdullah <aahh50018@gmail.com>
2023-11-12 20:27:14 +0300

    first commit

---
+++ starter.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL UNIQUE,
+  `name` text NOT NULL,
+  `password` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
