9b2dd6a0d97cecaab541d1e900c4390c95c867e5
Ali Al-Abdullah <aahh50018@gmail.com>
2025-01-26 15:42:00 +0300

    update

--- views/Home.view.php
+++ views/Home.view.php
@@ -3,8 +3,8 @@
 <div class="welcome">
     <h1>Ali Framework</h1>
     <p>Quick Start with your small project</p>
-    <a href="https://framework.ali-hussain.sa/">Read Documentation</a>
-    <a href="https://ali-hussain">By : Ali Hussain</a>
+    <a target="_blank" href="https://framework.ali-hussain.sa/">Read Documentation</a>
+    <a target="_blank" href="https://ali-hussain.sa">By : Ali Hussain</a>
 </div>
 
 <?php component("footer") ?>
